b251e37acdddf500a6279d6734fe66f4e753fc79
Million Yemane <wedi_ngus@yahoo.com>
2018-02-28 11:12:27 -0600

    yesterday work

---
+++ dist/app.js
@@ -0,0 +1,22 @@
+(function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
+
+},{}],2:[function(require,module,exports){
+arguments[4][1][0].apply(exports,arguments)
+},{"dup":1}],3:[function(require,module,exports){
+arguments[4][1][0].apply(exports,arguments)
+},{"dup":1}],4:[function(require,module,exports){
+"use strict";
+},{}],5:[function(require,module,exports){
+"use strict";
+let startup = document.getElementById("starest");
+console.log("starest", startup);
+
+
+let cakeGrid = require('./card-grid'),
+   foodieList = require('./foodie-list'),
+    home = require('./home'),
+    map = require('./map'),
+    load = require('./load');
+},{"./card-grid":1,"./foodie-list":2,"./home":3,"./load":4,"./map":6}],6:[function(require,module,exports){
+arguments[4][1][0].apply(exports,arguments)
+},{"dup":1}]},{},[5]);
---
+++ js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+let startup = document.getElementById("starest");
+console.log("starest", startup);
+
+
+let cakeGrid = require('./card-grid'),
+   foodieList = require('./foodie-list'),
+    home = require('./home'),
+    map = require('./map'),
+    load = require('./load');
